7dbbba431295ffc500d04b8db93ce3a2129a5f0a
xpike4 <x_pike@hotmail.com>
2013-07-15 14:02:11 -0500

    web.js actualizado

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require("fs");
+fs.readFile(path.join(process.cwd(), 'index.html'), function(err, data) {
+if (err) throw err;
+console.log(data.toString());
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
